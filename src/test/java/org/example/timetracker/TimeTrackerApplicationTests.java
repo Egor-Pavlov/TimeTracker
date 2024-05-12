4c1c31a07f6ea397a0aa24ae6d249174d93180b2
Егор Павлов <egor12200@gmail.com>
2024-05-12 09:00:57 +0700

    Initial commit

---
+++ src/test/java/org/example/timetracker/TimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.timetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
